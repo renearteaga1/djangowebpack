0f8d8badc4d544b019fbc1ca67fd5bb97c43a33f
Rene <renev88@gmail.com>
2020-03-09 13:20:25 -0500

    2 commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const path =require('path')
 
-module.export = {
-    entry: ''
+module.exports = {
+    entry: './static/js/script.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/js')
+    },
+    mode: 'development',
 }
